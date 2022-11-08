a705d85b373e447e5643454bb9082e5dfe54f1c8
Fatih Saridag <fatihsaridag26@gmail.com>
2022-11-09 00:06:02 +0300

    Tur Rehberleri listesi(45)

---
+++ TraversalCore.Mvc/Areas/Member/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalCore.Data.EntityFramework.Repository.Concrete;
+using TraversalCore.Services.Abstract;
+using TraversalCore.Services.Concrete;
+
+namespace TraversalCore.Mvc.Areas.Member.ViewComponents.MemberDashboard
+{
+    public class _GuideList: ViewComponent
+    {
+
+        GuideManager guideManager = new GuideManager(new EfGuideRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var guideList = guideManager.TGetList();
+            return View(guideList);  
+        }
+    }
+}
