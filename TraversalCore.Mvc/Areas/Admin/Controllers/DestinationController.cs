7f75f71d71b3d34d4a46ef0fa6bcdfa2c54e9ede
Fatih Saridag <fatihsaridag26@gmail.com>
2022-11-13 12:34:56 +0300

    Admin Paneli Side Bar Iconları Eklendi

--- TraversalCore.Mvc/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCore.Mvc/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TraversalCore.Data.EntityFramework.Repository.Concrete;
+using TraversalCore.Entity.Concrete;
+using TraversalCore.Services.Concrete;
 
 namespace TraversalCore.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
         public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var destination = destinationManager.TGetById(id);
+            destinationManager.TDelete(destination);
+            return RedirectToAction("Index");
+
+        }
+
+
+        [HttpGet]
+        public  IActionResult UpdateDestination(int id)
+        {
+            var destination = destinationManager.TGetById(id);
+            return View(destination);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
+        }
+
     }
 }
