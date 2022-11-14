3a61772d777356b0d37a7bb7c2627ba3e51fa1b2
Fatih Saridag <fatihsaridag26@gmail.com>
2022-11-14 19:03:47 +0300

    Admin Dashboard ViewComponentler başarıyla tamamlandı

---
+++ TraversalCore.Mvc/Areas/Admin/ViewComponents/_DestinationStatistic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Mvc.Areas.Admin.ViewComponents
+{
+    public class _DestinationStatistic : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCore.Mvc/Areas/Admin/ViewComponents/_TotalRevenue.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Mvc.Areas.Admin.ViewComponents
+{
+    public class _TotalRevenue : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
